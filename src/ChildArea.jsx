764e6a4e06a6043c839d3fd8001de009551b36a8
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-16 13:34:18 +0900

    React2.section3

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,12 +1,27 @@
-export const ChildArea = (props) => {
-  const { open } = props;
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "orange"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (
-        <div>
+        <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
